87213b8e30ea262aa04b2543e8265d26263a5714
RuiJ72 <rlagos900@gmail.com>
2022-07-05 01:05:04 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+    public class Main {
+        public static void main(String args[]) {
+            int contador = 0;
+            while (contador < 3) {
+                System.out.println("Repetição nr: " + contador);
+                contador++;
+
+                System.out.println("contador");
+            }
+        }
+    }
